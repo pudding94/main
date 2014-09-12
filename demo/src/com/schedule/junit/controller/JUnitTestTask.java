d5ef830c6dbedb78200c576f952f4123a84ececb
jinxlc <jinxlc@jinxlc.ufsoft.com.cn>
2014-09-12 14:41:17 +0800

    add task

--- demo/src/com/schedule/junit/controller/JUnitTestTask.java
+++ demo/src/com/schedule/junit/controller/JUnitTestTask.java
@@ -3,10 +3,12 @@ package com.schedule.junit.controller;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
-@Controller
+@Controller("taskJob") 
 public class JUnitTestTask {
-	@Scheduled(fixedRate=5000)
+//	@Scheduled(fixedRate=5000)
+	@Scheduled(cron="0/5 * *  * * ? ")
 	public void doSomething() {
 	    // something that should execute periodically
+		System.out.println("task");
 	}
 }
