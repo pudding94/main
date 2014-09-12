787d3f21dd8f3d44016986baf0f3a35705145791
jinxlc <jinxlc@jinxlc.ufsoft.com.cn>
2014-09-12 15:22:43 +0800

    commit

--- demo/src/com/schedule/junit/controller/JUnitTestTask.java
+++ demo/src/com/schedule/junit/controller/JUnitTestTask.java
@@ -1,14 +1,30 @@
 package com.schedule.junit.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
+import com.schedule.junit.service.JunitTestService;
+
 @Controller("taskJob") 
 public class JUnitTestTask {
-//	@Scheduled(fixedRate=5000)
+	private JunitTestService junitTestService;
+	
+	public JunitTestService getJunitTestService() {
+		return junitTestService;
+	}
+	@Resource
+	public void setJunitTestService(JunitTestService junitTestService) {
+		this.junitTestService = junitTestService;
+	}
+
+	//	@Scheduled(fixedRate=5000)
 	@Scheduled(cron="0/5 * *  * * ? ")
 	public void doSomething() {
 	    // something that should execute periodically
 		System.out.println("task");
+		String result=this.getJunitTestService().execute("w");
+		System.out.println("task:="+result);
 	}
 }
